63d8f1d847a06d4a9312cd046045cef4e247c21d
AbiollaghJames <abiollajames@proonmail.com>
2023-07-31 12:17:56 +0300

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from models import storage
+from os import getenv
 from api.v1.views import app_views
 
 # app variable, instance of Flask
@@ -18,4 +19,11 @@ def tear_down(self):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000,)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
